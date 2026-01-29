25c69da9f525f169a276eb53cfdc47d3a2f8f038
Sidd349 <ksiddhant962@gmail.com>
2026-01-29 08:04:46 -0500

    Task 1 Completed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,21 +3,21 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
   reducers: {
+
     addItem: (state, action) => {
-    
+      state.items.push(action.payload);
     },
+
     removeItem: (state, action) => {
     },
-    updateQuantity: (state, action) => {
 
-    
+    updateQuantity: (state, action) => {
     },
   },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
 export default CartSlice.reducer;
